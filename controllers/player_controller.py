2e72e7ef26c91deb65aed5934107400993206bf3
FloJouff <fenryll01@gmail.com>
2024-02-07 15:00:41 +0100

    Mise à jour player

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,3 +1,24 @@
+from views.player_view import PlayerView
+from models.player import Player
 
-"""Gère les actions liées aux joueurs:
-ajout, affichage, etc."""
+
+class PlayerController:
+    def __init__(self) -> None:
+        self.playerview = PlayerView()
+
+    def run_player(self,):
+        choix = ""
+        while (choix != "0"):
+            choix = self.playerview.menu_player()
+            if choix == "1":
+                data = self.playerview.get_player_infos()
+                print(data)
+                player = Player(data["name"], data["surname"], data["gender"],
+                                data["date_of_birth"])
+                player.save()
+            elif choix == "2":
+                players = Player.load_all_players()
+                self.playerview.afficher_list_players(players)
+            elif choix == "0":
+                print("Quitter")
+                break
--- views/player_view.py
+++ views/player_view.py
@@ -1,4 +1,29 @@
-from models import player
+from models.player import Player
+
+
+class PlayerView:
+    def afficher_player(self):
+        pass
+
+    def afficher_list_players(self, players: [Player]):
+        print("Afficher tous les joueurs: ")
+        for player in players:
+            print(player['name'], player['surname'])
+
+    def get_player_infos(self):
+        print("Rentrer les infos des joueurs: ")
+        name = input("Nom du joueur:")
+        surname = input("Prénom: ")
+        gender = input("Sexe: ")
+        date_of_birth = input("Date de naissance: ")
+        return {name, surname, gender, date_of_birth}
+
+    def menu_player(self):
+        print("Menu Player")
+        print("1. Pour Ajouter un joueur")
+        print("2. Pour lister tous les joueurs")
+        print("0. Quitter")
+        return input("Votre choix: ")
 
 
 """Affiche les informations liées aux joueurs ainsi
