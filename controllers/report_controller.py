cb684b93dfe7ffdac86df856380e8c70822143f7
FloJouff <fenryll01@gmail.com>
2024-02-28 16:34:17 +0100

    ajout des rapports

--- controllers/report_controller.py
+++ controllers/report_controller.py
@@ -1,6 +1,7 @@
 from views.report_view import ReportView
 from models.report import Report
 from models.player import Player
+from operator import attrgetter
 import json
 """ Gère la génération des rapports demandés"""
 
@@ -15,30 +16,97 @@ class ReportController:
             choix = self.reportview.menu_report()
             if choix == "1":
                 data = Report.display_tournaments()
-                print(data)
+                for i in range(len(data)):
+                    datas = [data[i]["id"], data[i]["name"], data[i]["place"],
+                             data[i]["start_date"], data[i]["end_date"],
+                             data[i]["players"], data[i]["description"],
+                             data[i]["number_of_round"], data[i]["rounds"],
+                             data[i]["status"]]
+                    filename = "tournois"
+                    fieldnames = ["id", "nom", "lieu", "start_date",
+                                  "end_date", "players", "description",
+                                  "Nb_of_round", "rounds", "status"]
+                    Report.add_data_to_csv(filename, fieldnames, datas)
             elif choix == "2":
                 data = Player.load_all_players()
-                print("Liste de tous les joueurs enregistrés dans le fichier: ")
+                print("Liste de tous les joueurs enregistrés dans le fichier:")
+                data = sorted(data, key=attrgetter('name'))
                 print(data)
                 print("")
+                for i in range(0, len(data)):
+                    datas = [data[i].id, data[i].name, data[i].first_name,
+                             data[i].gender, data[i].date_of_birth,
+                             data[i].ine]
+                    filename = "players"
+                    fieldnames = ["id", "nom", "prénom", "sexe",
+                                  "date de naissance", "ine"]
+                    Report.add_data_to_csv(filename, fieldnames, datas)
             elif choix == "3":
-                place = input("Pour quel tournoi souhaitez vous avoir ces informations? ")
-                data = Report.load_tournament_by_place(place)
-                print(data)
+                print("Liste des tournois: ")
+                tournoi = Report.display_tournaments()
                 print("")
+                choice = input("Pour quel tournoi souhaitez vous ces informations? ")
+                id_tournoi = tournoi[int(choice)]["id"]
+                data = Report.load_tournament_by_id(id_tournoi)
+                print("")
+                print("Nom du tournoi :", data.name, "Date de début: ",
+                      data.start_date, "Date de fin: ", data.end_date)
+                print("")
+                datas = [data.id, data.name, data.start_date, data.end_date]
+                filename = data.name
+                fieldnames = ["id", "nom", "Date de début", "Date de fin"]
+                Report.add_data_to_csv(filename, fieldnames, datas)
             elif choix == "4":
-                place = input("De quel tournoi souhaitez vous avoir la liste des participants? ")
-                data = Report.get_tournament_participants_by_place(place)
-                print(data)
+                print("Liste des tournois: ")
+                tournoi = Report.display_tournaments()
+                print("")
+                choice = input("Pour quel tournoi souhaitez vous ces informations? ")
+                id_tournoi = tournoi[int(choice)]["id"]
+                data = Report.get_tournament_participants_by_id(id_tournoi)
+
+                players = []
+                for player in data:
+                    joueur = Player.load_player_by_id(player)
+                    print("Joueur: ", joueur.name)
+                    players.append(joueur.name)
+                    datas = [joueur.id, joueur.name, joueur.first_name,
+                             joueur.ine]
+                    print("d:", datas)
+                    datas = datas.sort(key=data[1])
+                    filename = "Joueurs", tournoi[int(choice)]["name"]
+                    fieldnames = ["id", "nom", "prénom", "ine"]
+                    Report.add_data_to_csv(filename, fieldnames, datas)
+                players.sort()
+                print("Participants :", players)
                 print("")
             elif choix == "5":
-                place = input("De quel tournoi souhaitez vous connaitre le nombre de tours? ")
-                data = Report.get_tournament_numb_of_round_by_place(place)
-                print(f"Nombre de tours du tournoi de {place} :", data)
+                print("Liste des tournois: ")
+                tournoi = Report.display_tournaments()
+                print("")
+                choice = input("Pour quel tournoi souhaitez vous ces informations? ")
+                id_tournoi = tournoi[int(choice)]["id"]
+                data = Report.get_tournament_numb_of_round_by_id(id_tournoi)
+                datas = [tournoi[int(choice)]["id"],
+                         tournoi[int(choice)]["name"],
+                         tournoi[int(choice)]["number_of_round"]]
+                filename = tournoi[int(choice)]["name"], "nb_tour"
+                fieldnames = ["id", "name", "nb_tour"]
+                Report.add_data_to_csv(filename, fieldnames, datas)
+                print(f"Nombre de tours du tournoi de {tournoi[int(choice)]["name"]} :", data)
                 print("")
             elif choix == "6":
-                place = input("De quel tournoi souhaitez vous connaitre les résultats? ")
-                pass
+                print("Liste des tournois: ")
+                tournoi = Report.display_tournaments()
+                print("")
+                choice = input("Pour quel tournoi souhaitez vous ces informations? ")
+                id_tournoi = tournoi[int(choice)]["id"]
+                round = input("De quel tour voulez vous les matchs?")
+                data = Report.display_matches_per_round(id_tournoi, round)
+                datas = [tournoi[int(choice)]["id"],
+                         tournoi[int(choice)]["name"], round, data]
+                filename = tournoi[int(choice)]["name"], f"round{round}", "Matchs"
+                fieldnames = ["id", "name", "round", "matchs"]
+                Report.add_data_to_csv(filename, fieldnames, datas)
             elif choix == "0":
                 print("Quitter")
                 break
--- models/report.py
+++ models/report.py
@@ -1,19 +1,49 @@
 from models.tournament import Tournament
+from models.player import Player
 import json
+import os
+import csv
 
-"""
-dans les rapports:
-listes joueurs par ordre alphabétiques
-listes des tournois
-listes des joueurs d'un tournoi
-liste de tous les tours d'un tournoi et de tous les matchs d'un tour
-nom et date de tous les tournois
+# def load_tournament_start(self):
+#         with open("data/tournaments.json", "r") as f:
+#             data = json.load(f)
 
---> exporté en .txt ou .html
-"""
+#         tournament = []
+#         for d in data:
+#             if d["status"] == str("tostart"):
+#                 tournament.append(d)
+#         print("Afficher tous les tournois non démarrés: ")
+
+#         i = 0
+#         for t in tournament:
+#             print(i, t["name"], t["place"], t["status"])
+#             i = i + 1
+
+#         choix = input("Saissez le numéro du tournoi à démarrer: ")
+#         tournoi = tournament[int(choix)]
+
+#         return tournoi
 
 
 class Report:
+    def add_data_to_csv(file_name, fieldnames, data):
+        os.makedirs('fichiers_csv', exist_ok=True)
+        if os.path.isfile('fichiers_csv' + '//' + f'{file_name}.csv'):
+            with open('fichiers_csv' + '//' + f'{file_name}.csv', 'a',
+                      encoding='UTF-8-sig') as fichier_csv:
+                writer = csv.writer(fichier_csv, delimiter=",",
+                                    lineterminator='\n')
+                writer.writerow(data)
+        else:
+            fieldname = fieldnames
+
+            with open('fichiers_csv' + '//' + f'{file_name}.csv', 'a',
+                      encoding='UTF-8-sig') as fichier_csv:
+                writer = csv.writer(fichier_csv, delimiter=",",
+                                    lineterminator='\n')
+                writer.writerow(fieldname)
+                writer.writerow(data)
+
     def all_players_list_report():
         with open("data/player_data.json", "r") as f:
             data = json.loads(f.read())
@@ -22,53 +52,72 @@ class Report:
     def display_tournaments():
         with open("data/tournaments.json", "r") as f:
             data = json.loads(f.read())
-            print(data)
 
-    def load_tournament_by_place(place):
+        tournament = []
+        for d in data:
+            tournament.append(d)
+        print("")
+        i = 0
+        for t in tournament:
+            print(i, "Nom: ", t["name"], "Lieu: ", t["place"],
+                  "Date de début :", t["start_date"],
+                  "Date de fin :", t["end_date"],
+                  "Description :", t["description"])
+            i = i + 1
+        return data
+
+    def load_tournament_by_id(id):
         tournament = None
         with open("data/tournaments.json", "r") as f:
             data = json.load(f)
             for tournament_dict in data:
-                if tournament_dict['place'] == place:
-                    tournament = print(f"Le tournoi {tournament_dict["name"]}, situé à {tournament_dict["place"]}, a eu lieu du {tournament_dict["start_date"]} au {tournament_dict["end_date"]}")
+                if tournament_dict['id'] == id:
+                    tournament = Tournament(tournament_dict["name"],
+                                            tournament_dict["place"],
+                                            tournament_dict["start_date"],
+                                            tournament_dict["end_date"],
+                                            tournament_dict["description"],
+                                            tournament_dict["players"],
+                                            tournament_dict["rounds"]
+                                            )
                     break
         return tournament
 
-    def get_tournament_participants_by_place(place):
+    def get_tournament_participants_by_id(id):
         tournament = None
         with open("data/tournaments.json", "r") as f:
             data = json.load(f)
             for tournament_dict in data:
-                if tournament_dict['place'] == place:
+                if tournament_dict['id'] == id:
                     tournament = tournament_dict['players']
                     break
         # tournament.sort()
         return tournament
 
-    def get_tournament_numb_of_round_by_place(place):
+    def get_tournament_numb_of_round_by_id(id):
         tournament = None
         with open("data/tournaments.json", "r") as f:
             data = json.load(f)
             for tournament_dict in data:
-                if tournament_dict['place'] == place:
+                if tournament_dict['id'] == id:
                     tournament = tournament_dict['number_of_round']
                     break
         return tournament
 
-    def display_matches_per_round(name, round_nb):
+    def display_matches_per_round(id, round_nb):
         tournament = None
         with open("data/tournaments.json", "r") as f:
             data = json.load(f)
             for tournament_dict in data:
-                if tournament_dict['name'] == name & tournament_dict['round_nb'] == round_nb:
-                    tournament = tournament_dict['matches']
-                    break
+                if tournament_dict['id'] == id:
+                    for round_dict in tournament_dict['rounds']:
+                        if round_dict["round"] == int(round_nb):
+                            print(f"Matchs du tour {round_nb}:", round_dict['matchs'])
+                            for i in range(0, int(len(round_dict['matchs']))):
+                                for j in range(0, 2):
+                                    joueur = Player.load_player_by_id(round_dict['matchs'][i][j][0])
+                                    str(round_dict['matchs']).replace(str(round_dict['matchs'][i][j][0]), str(joueur))
+                                    print("joueurs id: ", round_dict['matchs'][i][j][0], "Nom:", joueur)
+                                    tournament = round_dict['matchs']
+                            break
         return tournament
-
-    # def save_matches():
-    #     with open("data/tournaments.json", "r") as f:
-    #         data = json.load(f)
-    #     tournament_data = Tournament.to_dict()
-    #     data.append(tournament_data)
-    #     with open("data/tournaments.json", "w") as f:
-    #         json.dump(data, f, indent=4)
